6cf681637e435aad19053034a3122109d3b68404
myusoof <mohmedyusoof.as@cognizant.com>
2013-08-14 03:34:49 +0100

    test

---
+++ src/main/groovy/firstClass.groovy
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yusoof
+ * Date: 8/14/13
+ * Time: 3:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class firstClass {
+    public static void main(String ...args){
+        println "test"
+    }
+}
