afb8729d3a94f6063822cef3a69a54a72b3bbaf3
myusoof <mohmedyusoof.as@cognizant.com>
2013-08-16 23:22:59 +0100

    [YM]firstClassImpl

---
+++ src/main/groovy/FirstClass.groovy
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yusoof
+ * Date: 8/14/13
+ * Time: 3:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class FirstClass {
+    public FirstClass(){
+
+    }
+    public String firstClassMethod(){
+        println "String from the first class method"
+    }
+}
--- src/main/groovy/FirstClassImpl.groovy
+++ src/main/groovy/FirstClassImpl.groovy
@@ -1,12 +1,13 @@
 /**
  * Created with IntelliJ IDEA.
  * User: yusoof
- * Date: 8/14/13
- * Time: 3:04 AM
+ * Date: 8/16/13
+ * Time: 11:13 PM
  * To change this template use File | Settings | File Templates.
  */
-class firstClass {
+class FirstClassImpl {
+    static FirstClass firstClass = new FirstClass()
     public static void main(String ...args){
-        println "test"
+        firstClass.firstClassMethod()
     }
 }
